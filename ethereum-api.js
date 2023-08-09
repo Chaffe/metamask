aac852b27863c411743045279658f4e3fd8aaf86
Mikhail <mikhail.m@gologin.com>
2023-08-09 10:59:11 +0400

    refactor: update rpc-url

--- ethereum-api.js
+++ ethereum-api.js
@@ -1,6 +1,6 @@
 import { Web3 } from 'web3';
 
-const PROVIDER_URL = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+const PROVIDER_URL = "https://mainnet.infura.io/v3/84ccc7f0d2974357a1d9f154a3e232ba";
 
 const web3 = new Web3(Web3.givenProvider || PROVIDER_URL);
 export const getTransaction = async (transactionHash) => web3.eth.getTransaction(transactionHash)
